839aa8de16f76f473fdcbaf601fd2d8bd6a66868
Hoanh An <hoanhan101@gmail.com>
2024-02-11 20:18:10 -0800

    feat: mainnet contract

--- scripts/playground.js
+++ scripts/playground.js
@@ -2,16 +2,18 @@ const fs = require("fs");
 
 async function main() {
   const factory = await ethers.getContractFactory("YourBrians");
-  const contract = factory.attach("0x45ac82bC0f12553e5144e1dBf7dba3A02865Bbec");
+  const contract = factory.attach("0xCDFCF5dbe963282ec31B71C1976e41c4FFe42652");
   console.log(await contract.name());
   console.log(await contract.totalSupply());
 
-  await contract.airdrop(
-    ["00ff00", "ff00ff"],
-    "0xd1b2672DFFF019efbA2b2056F64AF65BB30B077D"
-  );
+  await contract.setSender("0x59F61A131AA1BE988B5562504D94E54ee84Eab92");
 
-  console.log(await contract.totalSupply());
+  //   await contract.airdrop(
+  //     ["00ff00", "ff00ff"],
+  //     "0xd1b2672DFFF019efbA2b2056F64AF65BB30B077D"
+  //   );
+
+  //   console.log(await contract.totalSupply());
 }
 
 main().catch((error) => {
