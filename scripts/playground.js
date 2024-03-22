09c3b6ee628d8da2a95fe5398a291967bbed8f13
Hoanh An <hoanhan101@gmail.com>
2024-03-21 21:49:21 -0700

    feat: multiply

--- scripts/playground.js
+++ scripts/playground.js
@@ -1,20 +1,31 @@
+const path = require("path");
 const fs = require("fs");
+const utils = require("./utils");
 
 async function main() {
-  const factory = await ethers.getContractFactory("YourBrians");
-  const contract = factory.attach("0xCDFCF5dbe963282ec31B71C1976e41c4FFe42652");
-  console.log(await contract.name());
-  console.log(await contract.totalSupply());
-
-  await contract.setSender("0x59F61A131AA1BE988B5562504D94E54ee84Eab92");
-
-  //   await contract.airdrop(
-  //     ["00ff00", "ff00ff"],
-  //     "0xd1b2672DFFF019efbA2b2056F64AF65BB30B077D"
-  //   );
-
-  //   console.log(await contract.totalSupply());
+  const addresses = {
+    hardhat: "0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE",
+    sepolia: "0x8027db0d7bda7bd428a06be32305eb52fd0ebea5",
+    mainnet: "0xcdfcf5dbe963282ec31b71c1976e41c4ffe42652",
+  };
+  const contract = await getContract(addresses.sepolia);
+  await updateImg(contract);
 }
+
+const getContract = async (address) => {
+  const factory = await ethers.getContractFactory("YourBrians");
+  const contract = factory.attach(address);
+  console.log(await contract.name());
+  return contract;
+};
+
+const updateImg = async (contract) => {
+  const img = utils.blitmapEncode(
+    path.join(__dirname, "../assets/yourbrian_mutiply.svg")
+  );
+  console.log(img);
+  await contract.uploadArt(img);
+};
 
 main().catch((error) => {
   console.error(error);
